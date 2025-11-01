b2516b0b30392fbf84f38c8db7bb2241fa8314d0
Jesss1 <Kuo.kjess50@gmail.com>
2025-11-01 18:53:10 +0800

    first commit

---
+++ src/pages/HomePage_xx.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage_xx = () => {
+  return (
+    <div>
+      <h1>HomePage_xx</h1>
+      <h2>KuoChi-Ching, 211410278</h2>
+    </div>
+  );
+};
+
+export default HomePage_xx;
