0d371b6c9ec26ac7b8f041b12b8d6f7bf91b7eae
Jesss1 <Kuo.kjess50@gmail.com>
2025-11-01 20:26:00 +0800

    add tailwindcss support

--- src/pages/HomePage_xx.jsx
+++ src/pages/HomePage_xx.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const HomePage_xx = () => {
   return (
-    <div>
+    <div className="flex">
       <h1>HomePage_xx</h1>
       <h2>KuoChi-Ching, 211410278</h2>
     </div>
